ac66dc4c5e44a1cd55ff2a9fd739eb6ac2a3ea49
matheuspdf <matheuslopes.pdf@gmail.com>
2025-01-29 21:32:04 -0300

    Primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,62 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper
+          className="
+        pb-24 pt-10 lg:grid lg-grid-cols-3 
+        sm:pb-32 lg-gap-x-0 xl:gap-x-8 
+        lg:pt-24 xl-pt-32 lg-pb-52"
+        >
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div
+              className="relative mx-auto text-center lg:text-left
+            flex flex-col items-center lg-items-start"
+            >
+              <div className="absolute w-28 left-0 -top-0 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" />
+              </div>
+              <h1
+                className="relative w-fit tracking-tight text-balance mt-16 font-bold 
+              !leading-tight text-gray-900 text-5x1 md:text-6x1 lg:text-7x1"
+              >
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    High-quality, durable material     
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    5 year print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5"></div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
+
+// 28:58
